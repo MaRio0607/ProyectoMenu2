02fb13197941415683d15efe9d250233215c5694
Mario <57414832+MaRio0607@users.noreply.github.com>
2021-04-08 22:03:37 -0500

    Clase del 8 de abril(parte 2)

--- core/src/mx/mcd/demodvd/PantallaRunner.java
+++ core/src/mx/mcd/demodvd/PantallaRunner.java
@@ -1,20 +1,51 @@
 package mx.mcd.demodvd;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 
 public class PantallaRunner extends Pantalla {
+    private Juego juego;
+
+    //fondo infinito
+    private Texture textureFondo;
+    private float xFondo=0;
+
     public PantallaRunner(Juego juego) {
+        this.juego=juego;
     }
 
     @Override
     public void show() {
+        crearFondo();
 
+    }
+
+    private void crearFondo() {
+        textureFondo=new Texture("runner/fondoMario_5.jpg");
     }
 
     @Override
     public void render(float delta) {
-        borrarPantalla(1,0,1);
+        actualizar();
+        borrarPantalla(0,0,0);
+        batch.setProjectionMatrix(camara.combined);
 
+        batch.begin();
+        batch.draw(textureFondo,xFondo,0);
+        batch.draw(textureFondo,xFondo+textureFondo.getWidth(),0);
+        batch.end();
+
+    }
+
+    private void actualizar() {
+        actualizarFondo();
+    }
+
+    private void actualizarFondo() {
+        xFondo-=2;
+        if (xFondo <= -textureFondo.getWidth()){
+            xFondo= 0;
+        }
     }
 
     @Override
