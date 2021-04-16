bc655f253acaab696c3bb672c26a90be0886ad4e
Mario <57414832+MaRio0607@users.noreply.github.com>
2021-04-15 19:46:09 -0500

    Clase Mario

--- core/src/mx/mcd/demodvd/runner/Mario.java
+++ core/src/mx/mcd/demodvd/runner/Mario.java
@@ -1,9 +1,33 @@
 package mx.mcd.demodvd.runner;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import mx.mcd.demodvd.Objeto;
 
 /*
 Representa al pesonaje que esta en el escenario
  */
 public class Mario extends Objeto {
+    private Animation<TextureRegion> animacionCorrer;
+    private float timerAnimation; //sabre el frame que corresponde mostrar
+
+    public Mario(Texture texture, float x, float y){
+        TextureRegion region=new TextureRegion(texture);
+        TextureRegion[][]texturas= region.split(38,76);
+
+        //cuadros para caminar
+        TextureRegion[] arrFramesCaminar={ texturas[1][3],texturas[1][4], texturas[2][0]};
+        animacionCorrer= new Animation<TextureRegion>(0.5f,arrFramesCaminar);
+        animacionCorrer.setPlayMode(Animation.PlayMode.LOOP);
+        timerAnimation=0;
+
+        //IDLE
+        sprite=new Sprite(texturas[1][2]);
+        sprite.setPosition(x,y);
+    }
+
+
 }
